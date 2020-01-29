ac1aa3f75c4ef9f94fb21a4d7eba8f339e5a3b2f
채기운 <chaekiwoon@gmail.com>
2020-01-29 15:07:10 +0900

    Modify Ranking Data Type

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var csv = require('csv-parser');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,14 +13,22 @@ router.get('/game', function(req, res, next) {
 });
 
 router.get('/ranking', function(req, res, next) {
-  fs.readFile('ranking.txt', 'utf8', (err, ranking) => {
-    if(err) {
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    console.log(ranking);
-    res.render('ranking', {title: 'ranking', description: ranking});
-  });
+  var rank_data = [];
+
+  fs.createReadStream('ranking.csv')
+    .pipe(csv())
+    .on('data', (data) => rank_data.push(data))
+    .on('end', () => {
+      console.log(rank_data);
+    });
+  res.render('ranking', {title: 'ranking', rankarray: rank_data});
+  // fs.readFile('ranking.txt', 'utf8', (err, ranking) => {
+  //   if(err) {
+  //     console.log(err);
+  //     res.status(500).send('Internal Server Error');
+  //   }
+  //   console.log(ranking);
+  // });
 });
 
 router.get('/score', function(req, res, next) {
