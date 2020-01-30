a4d8412e75d2aedfc31a4cb3c92332333cefeae8
soohyeon <soohyeonie@naver.com>
2020-01-30 16:30:25 +0900

    sort csv

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
+const formidable = require('express-formidable');
 
 // var selectionRouter = require('./routes/selection');
 // var gameRouter = require('./routes/game');
@@ -22,6 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(formidable());
 
 app.use('/', indexRouter);
 
